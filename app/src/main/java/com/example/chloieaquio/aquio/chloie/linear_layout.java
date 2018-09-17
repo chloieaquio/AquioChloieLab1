4661f2e82797b8552aa8f1451e935c3e2b2204b8
Chloie Aquio <2015081043@ust-ics.mygbiz.com>
2018-09-17 19:45:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/chloieaquio/aquio/chloie/linear_layout.java
@@ -0,0 +1,13 @@
+package com.example.chloieaquio.aquio.chloie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class linear_layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_layout);
+    }
+}
